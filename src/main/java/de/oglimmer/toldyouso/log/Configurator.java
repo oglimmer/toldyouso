a123bf8c4c34f0ff945b7ec3ce21188d234a6929
Oli Zimpasser <oglimmer@gmail.com>
2016-08-20 14:40:44 +0200

    Move to SPI based slj4j config

---
+++ src/main/java/de/oglimmer/toldyouso/log/Configurator.java
@@ -0,0 +1,11 @@
+package de.oglimmer.toldyouso.log;
+
+import de.oglimmer.utils.BaseConfigurator;
+
+public class Configurator extends BaseConfigurator {
+
+	public Configurator() {
+		super("toldyouso");
+	}
+
+}
