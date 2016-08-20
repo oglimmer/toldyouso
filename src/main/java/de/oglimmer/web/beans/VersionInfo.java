658f055a3eabbbcd020cc250949c44a43ebe3784
Oli Zimpasser <oglimmer@gmail.com>
2016-08-20 14:41:11 +0200

    Move reading the version info to utils project

--- src/main/java/de/oglimmer/web/beans/VersionInfo.java
+++ src/main/java/de/oglimmer/web/beans/VersionInfo.java
@@ -1,16 +1,10 @@
 package de.oglimmer.web.beans;
 
-import java.io.InputStream;
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.jar.Attributes;
-import java.util.jar.Manifest;
-
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
-import javax.faces.context.FacesContext;
 import javax.inject.Named;
 
+import de.oglimmer.utils.VersionFromManifest;
 import lombok.Getter;
 
 @ApplicationScoped
@@ -22,23 +16,8 @@ public class VersionInfo {
 
 	@PostConstruct
 	public void init() {
-		String commit;
-		String version;
-		String creationDate;
-		try (InputStream is = FacesContext.getCurrentInstance().getExternalContext()
-				.getResourceAsStream("/META-INF/MANIFEST.MF")) {
-			Manifest mf = new Manifest(is);
-			Attributes attr = mf.getMainAttributes();
-			commit = attr.getValue("SVN-Revision-No");
-			version = attr.getValue("TYS-Version");
-			long time = Long.parseLong(attr.getValue("Creation-Date"));
-			creationDate = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT).format(new Date(time));
-		} catch (Exception e) {
-			commit = "?";
-			creationDate = "?";
-			version = "?";
-		}
-		longVersion = "V" + version + " [Commit#" + commit + "] build " + creationDate;
+		VersionFromManifest vfm = new VersionFromManifest("TYS");
+		longVersion = vfm.getLongVersion();
 	}
 
 }
